aef76f4256774ab6f423252fcc40cdc1607f4c73
GehrmanKosenkov <124575612+GehrmanKosenkov@users.noreply.github.com>
2024-07-21 13:10:06 +0300

    Delete Weather directory

--- Weather/2.Cleaner/Constants_for_smaller_files.py
+++
@@ -1,5 +0,0 @@
-
-stations_mapping = {
-    "Iskandar": "/Users/germankosenkov/Code projects/S&D estimation:forecasting/Indonesian S&D/New/Code/Data Preparation/Weather/2.Cleaner/Test/Iskandar",
-    "Japura": "/Users/germankosenkov/Code projects/S&D estimation:forecasting/Indonesian S&D/New/Code/Data Preparation/Weather/2.Cleaner/Test/Japura"
-}
