51857e09919907130cc6f5afc694b5f4cf1500d4
GehrmanKosenkov <124575612+GehrmanKosenkov@users.noreply.github.com>
2024-07-21 12:33:04 +0300

    Add files via upload

---
+++ Weather/2.Cleaner/Constants_for_smaller_files.py
@@ -0,0 +1,5 @@
+
+stations_mapping = {
+    "Iskandar": "/Users/germankosenkov/Code projects/S&D estimation:forecasting/Indonesian S&D/New/Code/Data Preparation/Weather/2.Cleaner/Test/Iskandar",
+    "Japura": "/Users/germankosenkov/Code projects/S&D estimation:forecasting/Indonesian S&D/New/Code/Data Preparation/Weather/2.Cleaner/Test/Japura"
+}
